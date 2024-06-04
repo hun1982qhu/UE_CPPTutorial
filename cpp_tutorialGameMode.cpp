32d44991a966857fa65ee67c2a8a22b52b78067d
H.N <hun1982@qq.com>
2024-06-05 01:59:50 +0200

    202406050159

--- cpp_tutorialGameMode.cpp
+++ cpp_tutorialGameMode.cpp
@@ -2,14 +2,16 @@
 
 #include "cpp_tutorialGameMode.h"
 #include "cpp_tutorialCharacter.h"
+#include "SandboxPawn.h"
 #include "UObject/ConstructorHelpers.h"
 
 Acpp_tutorialGameMode::Acpp_tutorialGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	// // set default pawn class to our Blueprinted character
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	// if (PlayerPawnBPClass.Class != NULL)
+	// {
+	// 	DefaultPawnClass = PlayerPawnBPClass.Class;
+	// }
+	DefaultPawnClass = ASandboxPawn::StaticClass();
 }
